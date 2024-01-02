e4a3f61df88ff017ac5da893bbdd07c02f70059a
jikky <kok5418@naver.com>
2024-01-02 18:46:10 +0900

    [Bronze III] Title: 공 넣기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,9 @@
+n,m=map(int,input().split())
+arr = [0]*n
+for i in range(m):
+    a,b,c=map(int,input().split())
+    for j in range(a-1,b):
+        arr[j] =c
+
+for i in arr:
+    print(i,end=" ")
