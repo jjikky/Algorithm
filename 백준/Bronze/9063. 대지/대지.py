48017ae434986be51e871c9c3bf209e5410938b4
jikky <kok5418@naver.com>
2024-01-09 19:18:52 +0900

    [Bronze III] Title: 대지, Time: 92 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,13 +1,10 @@
-x_max,y_max=-10000,-10000
-x_min,y_min=10000,10000
-for _ in range(int(input())):
-    x,y = map(int,input().split())
-    if x>x_max:
-        x_max=x
-    if x<x_min:
-        x_min=x
-    if y>y_max:
-        y_max=y
-    if y<y_min:
-        y_min=y
-print((x_max - x_min) * (y_max - y_min))
+import sys
+
+input = sys.stdin.readline
+x_list = []
+y_list = []
+for _ in range(int(input())) :
+    x, y = map(int, input().split())
+    x_list.append(x)
+    y_list.append(y)
+print((max(x_list) - min(x_list)) * (max(y_list) - min(y_list)))
