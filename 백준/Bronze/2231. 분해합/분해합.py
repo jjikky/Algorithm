0f7eb412be080a1b65d36572910fd491904b81d9
jikky <kok5418@naver.com>
2024-01-09 21:50:19 +0900

    [Bronze II] Title: 분해합, Time: 608 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,16 @@
+def convert(n):
+    result=n
+    while n>0:
+        result+=n%10
+        n//=10
+    return result
+    
+n=int(input())
+cnt=0
+for i in range(1,n+1):
+    if(convert(i)==n):
+        cnt+=1
+        print(i)
+        break
+if(cnt==0):
+    print(0)
