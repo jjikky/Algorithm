c555dfc4379489486fc12ac8086c24ae1614705b
jikky <kok5418@naver.com>
2024-01-09 18:03:10 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,12 +1,6 @@
-arr=[[],[]]
+x = y = 0
 for _ in range(3):
-    x,y = map(int,input().split())
-    if(x not in arr[0]):
-        arr[0].append(x)
-    else:
-        arr[0].remove(x)
-    if(y not in arr[1]):
-        arr[1].append(y)
-    else: arr[1].remove(y)
-    
-print(*arr[0],*arr[1])
+    a, b = map(int, input().split())
+    x^=a
+    y^=b
+print(x, y)
