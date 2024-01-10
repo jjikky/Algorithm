3a483669a6f5504463b4be0cbf783dc9911f3af6
jikky <kok5418@naver.com>
2024-01-10 15:51:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,10 +1,6 @@
 n=int(input())
-arr=[]
-for x in range(int(n/5)+1):
-    for y in range(int(n/3)+1):
-        if(5*x+y*3 == n):
-            arr.append(x+y)
-if(len(arr)==0):
-    print(-1)
-else:
-    print(min(arr))
+cnt=0
+while(n%5):
+    n-=3
+    cnt+=1
+print(n//5+cnt if n>=0 else -1)
