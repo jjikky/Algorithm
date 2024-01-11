b4964128e9b0b51d740a75d5b1e5325372d12288
jikky <kok5418@naver.com>
2024-01-11 19:11:55 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8820 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+input =sys.stdin.readline
+arr = [0]*10001
+
+for _ in range(int(input())):
+    arr[int(input())]+=1
+
+for i in range(10001):
+    if arr[i]!=0:
+        for _ in range(arr[i]):
+            print(i)
+            
