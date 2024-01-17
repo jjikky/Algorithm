abbafd5cd2008772005c53f1831919a510bb6679
jikky <kok5418@naver.com>
2024-01-17 16:34:59 +0900

    [Silver V] Title: 숫자 카드, Time: 400 ms, Memory: 125604 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,17 +1,4 @@
 input()
-arr1 = list(map(int,input().split()))
+arr = set(input().split())
 n=int(input())
-arr2=list(map(int,input().split()))
-result = [0]*n
-
-t=dict()
-for i in arr1:
-    t[i]=1
-
-for i in arr2:
-    try:
-        if t[i] ==1:
-            print(1, end=" ")
-    except:
-        print(0, end=" ")
-
+print(' '.join(['1' if i in arr else '0' for i in input().split()]))
