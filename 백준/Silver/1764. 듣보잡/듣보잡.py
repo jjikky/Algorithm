be73771118642dd0f6272a40e6f8c40f16a00450
jikky <kok5418@naver.com>
2024-02-11 22:00:23 +0900

    [Silver IV] Title: 듣보잡, Time: 80 ms, Memory: 45096 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,7 +1,7 @@
-n,m = map(int,input().split())
-n_set = set([input() for i in range(n)])
-m_set = set([input() for i in range(m)])
-result = n_set.intersection(m_set)
-print(len(result))
-for i in sorted(result):
-    print(i)
+from sys import stdin
+N,M=map(int,stdin.readline().split())
+l=stdin.read().splitlines()
+s1=set(l[:N])
+s2=set(l[N:])
+r=sorted(list(s1&s2))
+print(len(r),*r)
