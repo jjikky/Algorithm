ed69929d980e047ddbc345f5cb24e072368435e6
jikky <kok5418@naver.com>
2024-02-18 17:29:41 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,28 +1,17 @@
+import sys
 from collections import deque
-from sys import stdin
-input = stdin.readline
+t = int(sys.stdin.readline())
 
-t=int(input())
-for i in range(t):
-    n,index = map(int,input().split())
-    imp = deque(map(int,input().split()))
-    cnt=1
-    target = imp[index]
-    target_index=index
-    
-    while True:
-        max_v = max(imp)
-        if max_v == target and target_index==0: 
-            print(cnt)
+for _ in range(t):
+    n, idx = map(int, sys.stdin.readline().split())
+    li = list(map(int, sys.stdin.readline().split()))
+    que = deque([(i, li[i]) for i in range(len(li))])
+    li.sort(reverse=True)
+    cnt = 0
+    for el in li:
+        while que[0][1] != el:
+            que.append(que.popleft())
+        cnt += 1
+        if que.popleft()[0] == idx:
             break
-        if imp[0]==max_v:
-            if 0==target_index:
-                target_index+=len(imp)-2
-            else: target_index-=1
-            imp.popleft()
-            cnt+=1
-        else:
-            if 0==target_index:
-                target_index+=len(imp)-1
-            else: target_index-=1
-            imp.append(imp.popleft())
+    print(cnt)
