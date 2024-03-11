5bb470c42e1e9a949728f76bbc8a6dbfbee55932
jikky <kok5418@naver.com>
2024-03-11 12:21:49 +0900

    [level 0] Title: 문자열 묶기, Time: 9.88 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,7 +1,5 @@
 def solution(strArr):
-    arr = list(map(lambda x:len(x),strArr))
     dp=[0]*(len(strArr)+1)
-    for i in arr:
-        dp[i]+=1
-    answer = max(dp)
-    return answer
+    for i in strArr:
+        dp[len(i)]+=1
+    return max(dp)
