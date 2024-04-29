5bee13b12b2335821de6aeab29e58e6d9d357d6f
jikky <kok5418@naver.com>
2024-04-29 18:58:48 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,10 +1,14 @@
-from itertools import permutations
-
 n,m = map(int,input().split())
-data = [i+1 for i in range(n)]
+arr = []
 
-result = list(permutations(data,m))
-for i in result:
-    for j in i:
-        print(j, end=" ")
-    print("")
+def dfs():
+    if(len(arr)==m):
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,n+1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
