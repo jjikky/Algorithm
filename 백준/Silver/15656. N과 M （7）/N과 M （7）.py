7f9c78afd9d8fe9a89466549a903a3b10b6feb3e
jikky <kok5418@naver.com>
2024-04-29 19:30:29 +0900

    [Silver III] Title: N과 M (7), Time: 1600 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,15 @@
+n,m = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+
+arr = []
+
+def dfs():
+    if(len(arr)==m):
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(0,len(nums)):
+        arr.append(nums[i])
+        dfs()
+        arr.pop()
+
+dfs()
