a404e7cdc97befaf427833c2a086f90f31c00f7f
jikky <kok5418@naver.com>
2024-05-19 16:33:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,19 +1,8 @@
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-
-dp=[0]*(n+1)
-
-def sol(n):
-    if n==1:
-        return 1
-    elif n==2:
-        return 2
-    if dp[n]!=0:
-        return dp[n]
-    dp[n]=sol(n-1)+sol(n-2)
-    return dp[n]
+n=int(input())
+dp=[0]*1001
+dp[1]=1
+dp[2]=2
+for i in range(3,n+1):
+    dp[i]=dp[i-1]+dp[i-2]
     
-print(sol(n)%10007)
+print(dp[n]%10007)
