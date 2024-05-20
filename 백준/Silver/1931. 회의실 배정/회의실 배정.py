085733d5faeae8241f545248e5e6f99b3b2c9a55
jikky <kok5418@naver.com>
2024-05-20 22:33:43 +0900

    [Silver I] Title: 회의실 배정, Time: 276 ms, Memory: 59852 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,9 +1,11 @@
+import sys
+input = sys.stdin.readline
 n=int(input())
 time = sorted([list(map(int, input().split())) for _ in range(n)],key=lambda x:(x[1], x[0]))
-arr=[0]
+cnt=1
 k=0
 for i in range(1,n):
     if time[i][0]>=time[k][1]:
-        arr.append(i)
+        cnt+=1
         k=i
-print(len(arr))
+print(cnt)
