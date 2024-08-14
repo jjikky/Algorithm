3c3965b78d1875890e50ffadf347bb8936f4bc53
jikky <kok5417@skuniv.ac.kr>
2024-08-14 11:42:08 +0900

    [Silver V] Title: 색종이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,7 +1,32 @@
-arr=[0]*10000
-for _ in range(int(input())):
-    c,r = map(int,input().split())
-    for i in range(c,c+10):
-        for j in range(r,r+10):
-            arr[i*100+j]=1
-print(sum(arr))
+import sys
+
+n = int(input())
+
+arr=[]
+max_x,max_y=0,0
+# arr = [list(map(int,input().split())) for _ in range(n)]
+for i in range(n):
+    li = list(map(int,input().split()))
+    arr.append(li)
+    if li[0]>max_x:max_x=li[0]
+    if li[1]>max_y:max_y=li[1]
+    
+    
+answer = [[0]*(max_x+10) for _ in range(max_y+10)]
+
+# answer[25][17]
+
+# arr = [[3, 7], [15, 7], [5, 2]]
+for i in arr:
+    [x,y]=i
+    # i = [3,7]
+    for j in range(x,x+10):
+        for k in range(y,y+10):
+            answer[k][j]=1
+
+sum_answer=0
+for i in answer:
+    sum_answer+=sum(i)
+            
+        
+print(sum_answer)
