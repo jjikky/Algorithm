019aa85ed1310c0f28dc5d9f339dd6d6f6129bf1
jikky <kok5417@skuniv.ac.kr>
2024-08-19 09:32:00 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,10 +1,12 @@
-import sys
-n=int(input())
-l = sys.stdin.read().splitlines()
-def isVps(l):
-    while "()" in l :
-        l=l.replace('()','')
-    print("NO" if l else "YES")
+arr = [input() for _ in range(int(input()))]
 
-for i in l:
-    isVps(i)
+def isVps(s):
+    for _ in range(len(s)//2):
+        if not s: break
+        s=s.replace("()","")
+    if s:
+        return "NO"
+    return "YES"
+
+for s in arr:
+    print(isVps(s))
