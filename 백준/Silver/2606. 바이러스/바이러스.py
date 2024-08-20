99669f6121f31da3f1ebd9c679cdbffb26102780
jikky <kok5417@skuniv.ac.kr>
2024-08-20 09:50:33 +0900

    [Silver III] Title: 바이러스, Time: 52 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,28 @@
 from collections import deque
 
-def bfs(v):
-    queue = deque([v])
+def dfs(v):
+    q = deque([v])
     visited[v]=1
-    while queue:
-        v = queue.popleft()
+    while q:
+        now = q.popleft()
         for i in range(1,n+1):
-            if visited[i]==0 and graph[v][i]==1:
-                queue.append(i)
-                bfs(i)
-n=int(input())
-m=int(input())
-graph=[[0]*(n+1) for _ in range(n+1)]
-for i in range(m):
+            if visited[i]==0 and graph[v][i]!=0:
+                q.append(i)
+                dfs(i)
+        
+n = int(input())
+m = int(input())
+
+graph = [[0]*(n+1) for _ in range(n+1)]
+
+
+for _ in range(m):
     a,b = map(int,input().split())
     graph[a][b]=graph[b][a]=1
+
+# print(graph)
+
 visited=[0]*(n+1)
-bfs(1)
+
+dfs(1)
 print(sum(visited)-1)
